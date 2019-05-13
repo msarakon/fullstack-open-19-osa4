45010a819d03e18a67a79778fa646db53e551684
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-05-13 18:57:13 +0300

    4.4, 4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,7 +1,45 @@
 const listHelper = require('../utils/list_helper')
+const mockData = require('./mock_data')
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
 
 test('dummy should return one', () => {
   const blogs = []
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+  test('should return likes of the single blog', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('should return total likes for a list of multiple blogs', () => {
+    const result = listHelper.totalLikes(mockData.blogs)
+    expect(result).toBe(36)
+  })
+})
+
+describe('favorite blog', () => {
+
+  test('should return the single blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('should return the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(mockData.blogs)
+    expect(result._id).toBe('5a422b3a1b54a676234d17f9')
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,11 @@
-const dummy = (blogs) => {
-  console.log(blogs)
-  return 1
+const dummy = () => 1
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy }
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, blog) => fav.likes > blog.likes ? fav : blog, {})
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
