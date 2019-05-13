69903d7e94c0897b6dcb952f21638e91aaca826c
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-05-13 18:41:33 +0300

    part 4.3 step 1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 
 const mongoUrl = process.env.MONGODB_URI
@@ -14,7 +14,4 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogRouter)
 
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT, () => console.log(`Server running on port ${config.PORT}`))
